f89eb85abd4dca3cd1c72d9601b53613633e2489
MEsumeet <mahajansumeet79@gmail.com>
2022-10-11 00:40:20 +0530

    --init

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+$('.carousel').carousel({
+    interval: 3000,
+  });
+
+$('.collapse').collapse();
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
+
