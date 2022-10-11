45c6b3fb3285a56499d23f076effcaa4daf81fcf
MEsumeet <mahajansumeet79@gmail.com>
2022-10-12 00:15:05 +0530

    Owl updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,22 +2,25 @@ $('.carousel').carousel({
     interval: 3000,
   });
 
-$('.collapse').collapse();
 
 $('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:10,
-  nav:true,
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:3
-      },
-      1000:{
-          items:5
-      }
-  }
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
 })
 
